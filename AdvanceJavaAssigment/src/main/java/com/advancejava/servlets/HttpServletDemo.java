6247dd709c160a165d470cff29dcdabf299c30dc
Palash Bajaj <86784927+palashbajaj16@users.noreply.github.com>
2021-08-07 21:32:30 +0530

    Added Advanced java Assignment Task 1
    Here we can see
    
    1) src folder :- In this folder we have created JSP and Servlet files
    2) War file
    3) Word Document :- In this file we can Added screen Shots & project Description

---
+++ AdvanceJavaAssigment/src/main/java/com/advancejava/servlets/HttpServletDemo.java
@@ -0,0 +1,20 @@
+package com.advancejava.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/httpservlet")
+public class HttpServletDemo extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html");		
+		System.out.println("Servic is call");	
+		out.println("Hello World");
+	}
+}
---
+++ AdvanceJavaAssigment/src/main/java/com/advancejava/servlets/NormalServlet.java
@@ -0,0 +1,46 @@
+package com.advancejava.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/normalservlet")
+public class NormalServlet implements Servlet {
+	@Override
+	public void destroy() {
+		System.out.println("Destroy Method Call");		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		System.out.println("Init Method Call");
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		System.out.println("Servic is call");
+		out.println("Hello World");
+	}
+}
